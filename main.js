3fd2e80a5d43ebccf10f0d3b80d925eaf7c63dc3
Dimon <dimon@Dimons-iMac.local>
2018-06-04 14:02:29 -0400

    css

--- main.js
+++ main.js
@@ -1,68 +1,61 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var path = require('path');
-var nodemailer = require('nodemailer');
-var bodyParser = require('body-parser');
-require('dotenv').config();
+var path = require("path");
+var nodemailer = require("nodemailer");
+var bodyParser = require("body-parser");
+require("dotenv").config();
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname + "/bower_components")));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use( express.static(path.join(__dirname + '/bower_components')));
-app.use(bodyParser.urlencoded({extended: true}));
-
-
-
-app.get('/', function(req, res){
-    res.sendFile('/index.html');
+app.get("/", function(req, res) {
+  res.sendFile("/index.html");
 });
-app.get('/form', function(req, res){
-  res.redirect('/')
-})
-app.post('/form', function (req, res) {
+app.get("/form", function(req, res) {
+  res.redirect("/");
+});
+app.post("/form", function(req, res) {
   let name = req.body.name;
   let email = req.body.email;
   let message = req.body.message;
-    if(name === ""){
-      console.log('name required')
-      return;
-    } else if(email === ""){
-      console.log('email required')
-      return;
-    } else if (message === ""){
-      return;
-      console.log('message required')
-    } else {
+  if (name === "") {
+    return console.log("name required");
+
+  } else if (email === "") {
+      return console.log("email required");
+
+  } else if (message === "") {
+      return console.log("message required");
+  } else {
       let mailOpts, smtpTrans;
       smtpTrans = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.GMAIL_USER,
-          pass: process.env.GMAIL_PASS
-        }
-      });
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
+      }
+    });
 
-      mailOpts = {
-        from: req.body.name + ' &lt;' + req.body.email + '&gt;',
-        to: process.env.GMAIL_USER,
-        subject: 'New message from dblot.io',
-        text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
-      };
-      smtpTrans.sendMail(mailOpts)
-      setTimeout(function(){ res.redirect('/') }, 1500)
-    }
-
-
+    mailOpts = {
+      from: req.body.name + " &lt;" + req.body.email + "&gt;",
+      to: process.env.GMAIL_USER,
+      subject: "New message from dblot.io",
+      text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+    };
+    smtpTrans.sendMail(mailOpts);
+    setTimeout(function() {
+      res.redirect("/");
+    }, 1500);
+  }
 });
 
-
-
-
-app.get('*', function(req, res) {
-  res.status(404).send('<h1>uh oh! page not found!</h1>');
+app.get("*", function(req, res) {
+  res.status(404).send("<h1>uh oh! page not found!</h1>");
 });
 
-var server = app.listen(process.env.PORT || 3000, function(){
-  console.log('Open http://localhost:3000 in the browser');
+var server = app.listen(process.env.PORT || 3000, function() {
+  console.log("Open http://localhost:3000 in the browser");
 });
